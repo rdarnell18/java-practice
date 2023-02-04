b58f01879e5bbff8072100ab6e26c9f5e23a1085
Rachelle Darnell <rachelle.l.darnell@gmail.com>
2023-02-04 13:09:27 -0700

    initial commit of files

---
+++ src/test/java/com/example/bianca_practice/BiancaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bianca_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiancaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
